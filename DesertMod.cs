486ebf3694f26d57a3c86536852ae6781564ee8b
unknown <Reetu Kontio>
2020-05-22 09:12:57 +0300

    music override woraround

--- DesertMod.cs
+++ DesertMod.cs
@@ -4,5 +4,10 @@ namespace DesertMod
 {
 	public class DesertMod : Mod
  	{
+		public override void UpdateMusic(ref int music, ref MusicPriority priority)
+		{
+			music = this.GetSoundSlot(SoundType.Music, "Sounds/Boss/DesertBossMusic");
+			priority = MusicPriority.BossLow;
+		}
 	}
 }
