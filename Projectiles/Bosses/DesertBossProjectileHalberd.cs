7709403b92cb6aa7a96ebac7939004fd55dff923
Reetu Kontio <reetu.kontio@gmail.com>
2020-06-01 19:29:22 +0300

    halberd movement polishing

--- Projectiles/Bosses/DesertBossProjectileHalberd.cs
+++ Projectiles/Bosses/DesertBossProjectileHalberd.cs
@@ -16,24 +16,24 @@ namespace DesertMod.Projectiles.Bosses
         private float alpha = 0.0f;
 
         private NPC npc;
-        private float windupRadians;
-        private float swingRadians;
-        private int windupWindow;
-        private int swingWindow;
+        private int[] windupWindow = new int[3];
+        private int[] swingWindow = new int[3];
+        private float currentSpeed = 0f;
+        private int rotationOffsetIncrement;
 
         // Halberd movement variables
-        private float windupSpeed = 1f;
-        private float windupAcceleration = 0f;
-        private float windupDeceleration = 0f;
-        private float windupAngle = 90f;
+        private float windupSpeed = 2f;
+        private float windupAcceleration = 0.1f;
+        private float windupDeceleration = 0.02f;
+        private float constantWindupSpeedDistance = 20f;
 
         private float swingSpeed = -15f;
-        private float swingAcceleration = 0f;
-        private float swingDeceleration = 0f;
-        private float swingAngle = 270f;
+        private float swingAcceleration = -1f;
+        private float swingDeceleration = -1f;
+        private float constantSwingSpeedDistance = 90f;
 
         private float distanceFromCenter = 150f;
-        private float swingRotationOffset = 0f;
+        private float swingRotationOffset = 90f;
 
         public override void SetStaticDefaults()
         {
@@ -68,53 +68,74 @@ namespace DesertMod.Projectiles.Bosses
 
         public override void AI()
         {
-            if (aiPhase == 0) CalculateValues();
+            if (aiPhase == 0) InitializeValues();
 
             double rad = halberdRotation * (Math.PI / 180);
 
-            if (aiPhase < windupWindow)
+            // Rotate around boss
+            projectile.position.X = npc.Center.X - (int)(Math.Cos(rad) * distanceFromCenter) - projectile.width / 2;
+            projectile.position.Y = npc.Center.Y - (int)(Math.Sin(rad) * distanceFromCenter) - projectile.height / 2;
+
+            // Rotate projectile sprite
+            Vector2 bossToProjectile = projectile.Center - npc.Center;
+            bossToProjectile.Normalize();
+            projectile.rotation = -(float)(Math.Atan2(-bossToProjectile.Y, bossToProjectile.X) + Math.PI);
+
+            // Modify speed depending on movement values
+            if (aiPhase < windupWindow[0])
             {
-                // Rotate around boss
-                projectile.position.X = npc.Center.X - (int)(Math.Cos(rad) * distanceFromCenter) - projectile.width / 2;
-                projectile.position.Y = npc.Center.Y - (int)(Math.Sin(rad) * distanceFromCenter) - projectile.height / 2;
-
-                // Rotate projectile sprite
-                Vector2 bossToProjectile = projectile.Center - npc.Center;
-                bossToProjectile.Normalize();
-                projectile.rotation = -(float)(Math.Atan2(-bossToProjectile.Y, bossToProjectile.X) + Math.PI);
-
+                currentSpeed += windupAcceleration;
+                halberdRotation += currentSpeed;
+            }
+            else if (aiPhase < windupWindow[1])
+            {
                 halberdRotation += windupSpeed;
             }
-
-            if (aiPhase > windupWindow)
+            else if (aiPhase < windupWindow[2])
             {
-                // Rotate around boss
-                projectile.position.X = npc.Center.X - (int)(Math.Cos(rad) * distanceFromCenter) - projectile.width / 2;
-                projectile.position.Y = npc.Center.Y - (int)(Math.Sin(rad) * distanceFromCenter) - projectile.height / 2;
-
-                // Rotate projectile sprite
-                Vector2 bossToProjectile = projectile.Center - npc.Center;
-                bossToProjectile.Normalize();
-                projectile.rotation = -(float)(Math.Atan2(-bossToProjectile.Y, bossToProjectile.X) + Math.PI);
-
-                halberdRotation += swingSpeed;
+                currentSpeed -= windupDeceleration;
+                halberdRotation += currentSpeed;
             }
-
-            if (aiPhase > windupWindow + swingWindow)
+            else if (aiPhase < swingWindow[0])
+            {
+                currentSpeed += swingAcceleration;
+                halberdRotation += currentSpeed;
+            }
+            else if (aiPhase < swingWindow[1])
+            {
+                halberdRotation += swingSpeed + rotationOffsetIncrement;
+            }
+            else if (aiPhase < swingWindow[2])
+            {
+                currentSpeed -= swingDeceleration;
+                halberdRotation += currentSpeed;
+            }
+            else
             {
                 projectile.timeLeft = 0;
-            }
+            } 
 
+            // Add tick to ai phase
             aiPhase++;
         }
 
-        private void CalculateValues()
+        private void InitializeValues()
         {
             npc = Main.npc[(int)projectile.ai[0]];
-            windupRadians = windupAngle * 180f / (float)Math.PI;
-            swingRadians = swingAngle * 180f / (float)Math.PI;
-            windupWindow = (int) (windupAngle / Math.Abs(windupSpeed));
-            swingWindow = (int) (swingAngle / Math.Abs(swingSpeed));
+
+            GetWindow(windupWindow, windupSpeed, windupAcceleration, windupDeceleration, constantWindupSpeedDistance);
+            GetWindow(swingWindow, swingSpeed, swingAcceleration, swingDeceleration, constantSwingSpeedDistance, windupWindow[2]);
+            rotationOffsetIncrement = rotationOffsetIncrement / swingWindow[1];
+        }
+
+        private void GetWindow(int[] window, float speed, float acceleration, float deceleration, float constDistance, int offset = 0)
+        {
+            float windupAccelerationTime = (speed / acceleration);
+            float windupDecelerationTime = (speed / deceleration);
+            float constantSpeedTime = (int)(constDistance / Math.Abs(speed));
+            window[0] = offset + (int)windupAccelerationTime;
+            window[1] = window[0] + (int)constantSpeedTime;
+            window[2] = window[1] + (int)windupDecelerationTime;
         }
     }
 }
