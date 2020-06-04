43403c0befccd2ac9437327c35186df53d3bf3a3
Reetu Kontio <reetu.kontio@gmail.com>
2020-06-04 11:12:33 +0300

    added new class for custom hitboxes

---
+++ Projectiles/SimpleHitbox.cs
@@ -0,0 +1,37 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace DesertMod.Projectiles
+{
+    public class SimpleHitbox : ModProjectile
+    {
+        public override bool CanHitPlayer(Player target)
+        {
+            return CanHit(target);
+        }
+
+        public override bool CanHitPvp(Player target)
+        {
+            return CanHit(target);
+        }
+
+        public override bool? CanHitNPC(NPC target)
+        {
+            return CanHit(target);
+        }
+
+        public bool CanHit(Entity entity)
+        {
+            if (Collides(entity))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public virtual bool Collides(Entity entity)
+        {
+            return true;
+        }
+    }
+}
