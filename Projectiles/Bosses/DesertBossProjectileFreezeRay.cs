490bd701f519f64b79223ad8a9a81d58a68a68b2
Reetu Kontio <reetu.kontio@gmail.com>
2020-06-25 22:56:51 +0300

    comments

--- Projectiles/Bosses/DesertBossProjectileFreezeRay.cs
+++ Projectiles/Bosses/DesertBossProjectileFreezeRay.cs
@@ -10,6 +10,7 @@ namespace DesertMod.Projectiles.Bosses
     class DesertBossProjectileFreezeRay : ModProjectile
     {
         int aiPhase = 0;
+
         Player target;
         NPC npc;
         Vector2 freezePos;
@@ -44,6 +45,7 @@ namespace DesertMod.Projectiles.Bosses
 
         public override void AI()
         {
+            // Initialize
             if (aiPhase == 0)
             {
                 npc = Main.npc[(int)projectile.ai[0]];
@@ -51,8 +53,10 @@ namespace DesertMod.Projectiles.Bosses
                 freezePos = target.position;
             }
 
+            // The projectilestays on target
             projectile.position = target.position;
 
+            // Freeze the target movement
             if (target != null)
             {
                 target.position = freezePos;
@@ -65,27 +69,36 @@ namespace DesertMod.Projectiles.Bosses
         {
             if (npc == null) return false;
 
+            int i = 0;
+            float accumulated = 0f;
             bool reached = false;
+
             Vector2 dir = projectile.Center - npc.Center;
             float distance = dir.Length();
             dir.Normalize();
-            int i = 0;
-            float acculumated = 0f;
+            
             Texture2D texture = ModContent.GetTexture("DesertMod/Projectiles/Bosses/DesertBossProjectileSpiritDagger");
             float offset = 90f / 180f * (float)Math.PI;
 
-            // Draw the tether
+            // Draw the sprites forming the tether
             while (!reached)
             {
+                // Accumulate
                 i++;
-                acculumated += texture.Height;
+                accumulated += texture.Height;
+
+                // Sprite parameters
                 Vector2 pos = projectile.Center - dir * texture.Height * i;
                 Rectangle? rec = new Rectangle?();
                 Color color = Color.White;
-                float rotation = -(float)(Math.Atan2(-dir.Y, dir.X) - offset);
+                float rotation = -(float)(Math.Atan2(-dir.Y, dir.X) - offset); // Rotate towards target
                 Vector2 origin = new Vector2(texture.Width * 0.5f, texture.Height * 0.5f);
+
+                // Draw sprite
                 ((SpriteBatch)Main.spriteBatch).Draw(texture, pos - Main.screenPosition, rec, color, rotation, origin, 1f, SpriteEffects.None, 0.0f);
-                if (acculumated + texture.Height > distance) reached = true;
+
+                // If the tether is long enough stop drawing
+                if (accumulated + texture.Height > distance) reached = true;
             }
             return true;
         }
--- Projectiles/Bosses/DesertBossProjectileSpiritDagger.cs
+++ Projectiles/Bosses/DesertBossProjectileSpiritDagger.cs
@@ -8,6 +8,7 @@ namespace DesertMod.Projectiles.Bosses
 {
     class DesertBossProjectileSpiritDagger : ModProjectile
     {
+        // How many ghosting sprites does the trail have
         int tailLength = 4;
 
         public override void SetStaticDefaults()
@@ -40,6 +41,7 @@ namespace DesertMod.Projectiles.Bosses
 
         public override void AI()
         {
+            // Glow and dust trail
             Dust.NewDust(projectile.position, projectile.width, projectile.height, 217,
                 projectile.velocity.X * 0.25f, projectile.velocity.Y * 0.25f, 150, default(Color), 1f);
             Lighting.AddLight(projectile.Center, 0.3f, 1f, 1f);
@@ -47,20 +49,25 @@ namespace DesertMod.Projectiles.Bosses
 
         public override bool PreDrawExtras(SpriteBatch spriteBatch)
         {
+            Texture2D texture = ModContent.GetTexture("DesertMod/Projectiles/Bosses/DesertBossProjectileSpiritDagger");
+            Vector2 dir = projectile.velocity;
+            dir.Normalize();
+
             // Draw the ghosting trail
             for (int i = 0; i < tailLength; i++)
             {
-                Texture2D texture = ModContent.GetTexture("DesertMod/Projectiles/Bosses/DesertBossProjectileSpiritDagger");
-                Vector2 dir = projectile.velocity;
-                dir.Normalize();
+                // Parameters
                 Vector2 pos = projectile.Center - dir * 15f * (i + 1);
                 Rectangle? rec = new Rectangle?();
                 float fadeout = 1f / ((float)i + 2f);
                 Color color = new Color(fadeout, fadeout, fadeout, fadeout);
                 float rotation = projectile.rotation;
                 Vector2 origin = new Vector2(texture.Width * 0.5f, texture.Height * 0.5f);
+
+                // Draw sprite
                 ((SpriteBatch)Main.spriteBatch).Draw(texture, pos - Main.screenPosition, rec, color, rotation, origin, 1f, SpriteEffects.None, 0.0f);
             }
+
             return true;
         }
     }
